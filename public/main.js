0dbdb422a9767bbff55702e5732e0d9a39166b0d
Michelle Hendrix <michelle49@mail.usf.edu>
2020-03-16 22:05:19 -0400

    scoreboard app

--- public/main.js
+++ public/main.js
@@ -1,7 +1,44 @@
+const updateTeamName = teamNumber => {
+  const input = document.querySelector(`.team-${teamNumber}-input`)
+  const title = document.querySelector(`.team-${teamNumber}-name`)
+  title.textContent = input.value
+}
+const updateScore = (teamNumber, scoreAdjustment) => {
+  const score = document.querySelector(`.team-${teamNumber}-score`)
+  const newScore = parseInt(score.textContent, 10) + scoreAdjustment
+  score.textContent = newScore
+}
+
+const setupEventListeners = () => {
+  document.addEventListener('click', e => {
+    e.stopPropagation()
+    switch (e.target.className) {
+      case 'update-team-1-name':
+        updateTeamName(1)
+        break
+      case 'update-team-2-name':
+        updateTeamName(2)
+        break
+      case 'team-1-subtract-1-button':
+        updateScore(1, -1)
+        break
+      case 'team-2-subtract-1-button':
+        updateScore(2, -1)
+        break
+      case 'team-1-add-1-button':
+        updateScore(1, 1)
+        break
+      case 'team-2-add-1-button':
+        updateScore(2, 1)
+        break
+      default:
+        break
+    }
+  })
+}
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  setupEventListeners()
 }
 
 document.addEventListener('DOMContentLoaded', main)
